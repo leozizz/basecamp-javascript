a72dc6a87b9f1308a13587b8b04c186600dc8572
Leo Silva <75532653+leozizz@users.noreply.github.com>
2021-10-26 17:42:00 -0300

    Código base do exercício
    Escrita com anotações do código base para teste de operadores.

--- sintaxe-e-operadores/comparaNumeros.js
+++ sintaxe-e-operadores/comparaNumeros.js
@@ -1,4 +1,5 @@
 function comparaNumeros(num1, num2) {
+	if(!num1 || !num2) return 'Defina dois números'
 	const primeiraFrase = criaPrimeiraFrase(num1, num2);
 	const segundaFrase = criaSegundaFrase(num1, num2);
 
@@ -6,30 +7,35 @@ function comparaNumeros(num1, num2) {
 }
 
 function criaPrimeiraFrase(num1, num2) {
-	let primeiraFrase = `Os números ${num1} e ${num2}`;
-	let simNao = 'não';
+	let saoIguais = '';
 
-	if (num1 === num2) {
-		simNao = '';
+	//IF irá testar se os números não são iguais, no caso, se for False "saoIguais" recebe "não"
+	if(num1 !== num2) { 
+		saoIguais = 'não';
 	}
-
-	return `${primeiraFrase} ${simNao} são iguais.`;
+	return `Os números ${num1} e ${num2} ${saoIguais} são iguais.`
 }
+
 
 function criaSegundaFrase(num1, num2) {
 	const soma = num1 + num2;
-	let comparaDez = 'menor';
-	let comparaVinte = 'menor';
 
-	if (soma > 10) {
-		comparaDez = 'maior';
+	let resultado10 = 'menor'
+	let resultado20 = 'menor'
+	const compara10 = soma > 10;
+	const compara20 = soma > 20;
+
+	// checa se a soma de "num1" e "num2" é maior que 10, retornando 'maior' para True e 'menor' para false
+	if (compara10) {
+		resultado10 = 'maior';
 	}
 
-	if (soma > 20) {
-		comparaVinte = 'maior';
+	// checa se a soma de "num1" e "num2" é maior que 20, retornando 'maior' para True e 'menor' para false
+	if (compara20) {
+		resultado20 = 'maior';
 	}
 
-	return `Sua soma é ${soma}, que é ${comparaDez} do que 10 e ${comparaVinte} do que 20.`;
+	return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.`;
 }
 
-console.log(comparaNumeros(10, 10));
+console.log(comparaNumeros(20,20));
