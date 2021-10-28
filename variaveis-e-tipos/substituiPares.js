a892ab97e69771a6dfc8046a026baa8cf958df8c
Leo Silva <75532653+leozizz@users.noreply.github.com>
2021-10-27 23:40:37 -0300

    Exercícios de sintaxe e operadores realizados.
    Teste para checar e trocar todos os elementos pares e diferentes de zero de um array pelo número 0 e caso o array seja vazio, retorne -1.

--- variaveis-e-tipos/substituiPares.js
+++ variaveis-e-tipos/substituiPares.js
@@ -1,18 +1,28 @@
-function substituiNumerosPares(array) {
-	if (!array.length) return console.log(-1);
+function substituiPares(array) {
+    if(!array) return -1;
+    if(!array.length) return -1;
 
-	const naoZero = (num) => num !== 0;
-	const numPar = (num) => num % 2 === 0;
+    /*
+    Para cada indice a partir de 0, desde que o indice seja menor que o length do array, 
+    vamos incrementar e verificar que se o elemento do indice do array for igual a 0, 
+    vamos mandar um console.log
+    */
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === 0) {
+            console.log("Você já é zero!")
+        }   else if (array[i] % 2 === 0) {
+            console.log(`Substituindo ${array[i]} por 0...`)
+            array[i] = 0; 
+            /*Se o restante da divisão for igual a zero (par)
+            vamos determinar que o array[i] vai ser 0.
+            */
+        }  
+    }
 
-	for (let i = 0; i < array.length; i++) {
-		if (numPar(array[i]) && naoZero(array[i])) {
-			console.log(`trocando ${array[i]} por 0...`);
-			array[i] = 0;
-		} else if (!naoZero(array[i])) {
-			console.log('Ops, você já vale 0!');
-		}
-	}
-	console.log(array);
+    return array;
 }
 
-substituiNumerosPares([2, 0, 3, 5]);
+let retorno = [2, 8, 11, 16, 77, 33, 323, 91];
+console.log(substituiPares(retorno));
+
+//Este exemplo deve retornar [0, 0, 11, 0, 77, 33, 323, 91]
