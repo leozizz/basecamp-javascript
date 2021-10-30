9781e71a22c719557d2f271491c622b2d9c87bb7
Leo Silva <75532653+leozizz@users.noreply.github.com>
2021-10-30 15:22:23 -0300

    Testes realizados
    Exercício realizado de acordo com o desafio proposto na aula de Maps e Sets

--- colecoes/sets.js
+++ colecoes/sets.js
@@ -1,8 +1,10 @@
-function uniqueElements(array) {
-	let unique = new Set(array);
-	return [...unique];
+const myArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
+
+function uniqueValues(arr) {
+    const mySet = new Set(arr)
+
+    //Aqui utilizamos o argumento hash para que os elementos do set se tornem um elemento do array
+    return [...mySet];
 }
 
-const arr = [30, 30, 40, 5, 223, 2049, 3034, 5];
-
-console.log(uniqueElements(arr));
+console.log(uniqueValues(myArray))
