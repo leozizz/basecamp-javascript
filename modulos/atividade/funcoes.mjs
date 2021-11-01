c9a4cc088a2f91f2630c83e6de8bc6bfa1149031
Leo Silva <75532653+leozizz@users.noreply.github.com>
2021-11-01 20:04:42 -0300

    Código e testes realizados
    Código escrito com base no exercício proposto de Módulos em JavaScript no bootcamp Impulso React

--- modulos/atividade/funcoes.mjs
+++ modulos/atividade/funcoes.mjs
@@ -1,11 +1,19 @@
-export function mostraIdade(nome, idade) {
-	return `A idade de ${nome} é ${idade}.`;
+//Abaixo funções apenas para teste
+function mostraIdade(nome, idade) {
+    return `A idade de ${nome} é ${idade}`
 }
 
-export function mostraCidade(nome, cidade) {
-	return `A idade de ${nome} é ${cidade}.`;
+function mostraCidade(nome, cidade) {
+    return `A cidade de ${nome} é ${cidade}`
 }
 
-export function mostraHobby(nome, hobby) {
-	return `A idade de ${nome} é ${hobby}!`;
+function mostraHobbie(nome, hobbie) {
+    return `O hobbie de ${nome} é ${hobbie}`
+}
+
+//Export das funções
+export {
+    mostraIdade,
+    mostraCidade,
+    mostraHobbie
 }
