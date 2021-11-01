103f15c1e512edc59ebcad67dcf9fff312a7b841
Leo Silva <75532653+leozizz@users.noreply.github.com>
2021-11-01 20:05:56 -0300

    Exercício e testes realizados
    Código escrito com base no exercício proposto de Módulos em JavaScript no bootcamp Impulso React

--- modulos/atividade/main.mjs
+++ modulos/atividade/main.mjs
@@ -1,7 +1,7 @@
-import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+//Import as funções do arquivo criado anteriormente
+import { mostraCidade, mostraIdade, mostraHobbie } from "./funcoes.mjs";
 
-console.log(mostraIdade('Marina', 20));
-
-console.log(mostraCidade('Silvia', 'São Paulo'));
-
-console.log(mostraHobby('Carol', 'Ouvir música'));
+//Ira retornar no console os dados informados
+console.log(mostraIdade("Leonardo", 22))
+console.log(mostraCidade("Leonardo", "Campinas"))
+console.log(mostraHobbie("Leonardo", "Jogar Dark Souls"))
