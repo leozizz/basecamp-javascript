952e9909fd3248ff8959785c1b81a0c533a58c83
Leo Silva <75532653+leozizz@users.noreply.github.com>
2021-11-02 18:00:56 -0300

    Exercício proposto
    Exercício para testar a filtragem de todos os números pares de impares e um array.

--- map-filter-reduce/filter/filtraPares.js
+++ map-filter-reduce/filter/filtraPares.js
@@ -1,9 +1,26 @@
+//Função para filtrar os numeros pares de um array
+
 function filtraPares(arr) {
-	if (!arr || !arr.length) return;
-
-	const filteredArr = arr.filter((item) => item % 2 === 0);
-
-	return filteredArr;
+    return arr.filter(callback)
 }
 
-console.log(filtraPares([1, 2, 3, 4]));
+//Função responsável pelo calculo
+function callback(item) {
+    return item % 2 === 0
+    //Opção para números impares > return item % 2 !== 0
+}
+
+//Só irá retornar os números que passarem pelo filtro
+const meuArray = [2, 10, 89, 32, 4, 91, 11]
+
+console.log(filtraPares(meuArray))
+
+/*
+Outra opção de sintaxe para essa função
+
+function filtraPares(arr) {
+    return arr.filter(function callback(item) {
+        return item % 2 === 0
+    })
+}
+*/
