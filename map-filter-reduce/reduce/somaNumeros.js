4210da03155650e17ac2a7f4dad962c7a1934793
Leo Silva <75532653+leozizz@users.noreply.github.com>
2021-11-02 18:07:31 -0300

    Exercicio proposto realizado
    Exercício que realiza uma função para somar todos os números de um array.

--- map-filter-reduce/reduce/somaNumeros.js
+++ map-filter-reduce/reduce/somaNumeros.js
@@ -1,8 +1,13 @@
-function somaNumeros(arr) {
-	if (!arr || !arr.length) return;
-	const soma = arr.reduce((prev, curr) => prev + curr);
+//Função que irá somar todos os números de um array
 
-	return soma;
+function somaNumeros(arr) {
+    return arr.reduce(function (prev, current) {
+        console.log({prev})
+        console.log({current})
+        return prev + current
+    })
 }
 
-console.log(somaNumeros([1, 1, 1, 3]));
+const arr = [1, 2]
+
+console.log(somaNumeros(arr))
